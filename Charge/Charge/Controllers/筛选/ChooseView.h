7cf656e1c77ef1e36bfbbf9fcc83756a65c5bece
luoyouyou <812656410@qq.com>
2018-11-07 22:09:33 +0800

    11

--- Charge/Charge/Controllers/筛选/ChooseView.h
+++ Charge/Charge/Controllers/筛选/ChooseView.h
@@ -2,14 +2,12 @@
 //  ChooseView.h
 //  Charge
 //
-//  Created by 罗小友 on 2018/10/31.
+//  Created by 罗小友 on 2018/11/7.
 //  Copyright © 2018 com.XinGuoXin.cn. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-typedef void(^DaohangClick)(NSString *a ,NSString *b);
 @interface ChooseView : UIView
--(instancetype)initWithFrame:(CGRect)frame;
-@property (nonatomic ,strong)DaohangClick daohang;
+
 @end
